002668d95263d804c6f542b38fa025895bb753a8
TheodoreCouz <74992568+TheodoreCouz@users.noreply.github.com>
2022-10-12 12:18:11 +0200

    Update Q1.py

--- H1/Q1.py
+++ H1/Q1.py
@@ -1,59 +1,43 @@
 import time
 import math
+import pandas as pd
 
 def ElecPowerCapa():
 
-    class Date:
-        #classe qui permet de stocker une date plus facilement à partir d'une string
-        # "yyyymmdd" =>  [date.yyyy, date.mm, date.dd]
-        def __init__(self, date):
-            self.year = int(date[:4])
-            self.month = int(date[4:6])
-            self.day = int(date[6:])
+    Beauvechain = pd.read_csv("Beauvechain.csv", sep=",")
+    Beauvechain=Beauvechain.loc[Beauvechain["    DATE"] >= 20170101].loc[Beauvechain["    DATE"] <= 20210101]
 
-        #return une rerprésentation de l'objet
-        def  __str__(self):
-            return(
-                str(self.year)+"/"+str(self.month)+"/"+str(self.day)
-            )
+    Elsenborn = pd.read_csv("Elsenborn.csv", sep=",")
+    Elsenborn=Elsenborn.loc[Elsenborn["    DATE"] >= 20170101].loc[Elsenborn["    DATE"] <= 20210101]
+    
+    ErrorsElsenborn=Elsenborn.loc[Elsenborn[" Q_FG"] != 0]
+    ErrorsBeauvechain=Beauvechain.loc[Beauvechain[" Q_FG"] != 0]
+
+    Elsenborn=Elsenborn[~Elsenborn["    DATE"].isin(ErrorsBeauvechain["    DATE"])]
+    Beauvechain=Beauvechain[~Beauvechain["    DATE"].isin(ErrorsElsenborn["    DATE"])]
+
+    Elsenborn=Elsenborn.loc[Elsenborn["   FG"] >=0]
+    Beauvechain=Beauvechain.loc[Beauvechain["   FG"] >=0]
+
+
+    print(Elsenborn)
 
     #constantes imposées dans l'énoncé
     PI = math.pi
     RHO = 1.2
     SPEEDLIMIT = 90/3.6 # en m/s
-    d1 = Date("20170101")
-    d2 = Date("20210101")
     Pk = 0
     beauvechain = None
     elsenborn = None
+    nDays=0
+    ItB=0 #itérateur pour beauvechain
+    ItE=0 #itérateur pour elsenborn
 
-    #return un bolléen en fonction de si <date> se trouve chronologiquement entre <d1> et <d2>
-    def IsBetween(date, d1, d2):
-        if date.year < d1.year or date.year > d2.year: return False
-        elif date.year == d2.year:
-            if date.month > d2.month or date.day > d2.day: return False
-            else: return True
-        else: return True
+ 
 
-    with open("Beauvechain.csv") as file1:
-        beauvechain = file1.readlines()[1:]
-    file1.close()
-
-    with open("Elsenborn.csv") as file2:
-            elsenborn = file2.readlines()[1:]
-    file2.close()
-
-    for line in beauvechain+elsenborn:
-        l = line.strip(" ").split(",") #formatage de la ligne
-        date = Date(l[1]) #utilisation de la classe Date définie ci-haut
-        speed = int(l[2])/10 #m/s
-        if int(l[3]) == 0: #vérifie que le code d'erreur ne renseigne pas d'erreur
-            if IsBetween(date, d1, d2): #vérifie que <date> est bien entre <d1> et <d2>
-                if speed >= 0 and speed <= SPEEDLIMIT: #vérifie que la vitesse en m/s n'excéde pas 90km/h et soit positive
-                    Pk += 0.5*RHO*((32**2)*PI)*speed**3/(10**6) #application de la formule de l'énoncé (en W)
     Pe = 0.42*Pk
     return Pe
 
 ##########################
 
-print("Answer Q1: "+str(ElecPowerCapa())+" Mega Watts")
+ElecPowerCapa()
