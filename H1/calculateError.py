5905fa187cf36ae616c8a91c81598ffd876af440
Thomas Rixen <thomas.rixen@student.uclouvain.be>
2022-10-20 18:17:31 +0200

    comments + finishing computing error

--- H1/calculateError.py
+++ H1/calculateError.py
@@ -1,27 +1,45 @@
+
+import re
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 from scipy import stats
-from Q4 import WindElsenborn
+from Q1 import ElecPowerCapa
 
+electroCapa = ElecPowerCapa()[0]
+etendue = round(max(ElecPowerCapa()[0]))
 
 #takes gamma parameters
-shapeGammaElsenborn = stats.gamma.fit(WindElsenborn)[0]
-locationGammaElsenborn = stats.gamma.fit(WindElsenborn)[1]
-scaleGammaElsenborn = stats.gamma.fit(WindElsenborn)[2]
-print("ShapeGammaElsenborn: {} \nLocationGammaElsenborn: {} \nScaleGammaElsenborn: {}\n".format(shapeGammaElsenborn, locationGammaElsenborn, scaleGammaElsenborn))
+shapeGammaElsenborn,locationGammaElsenborn,scaleGammaElsenborn  = stats.gamma.fit(electroCapa)
 
 
 #takes invert gauss parameters
-shapeInvGaussElsenborn = stats.invgauss.fit(WindElsenborn)[0]
-locationInvGaussElsenborn = stats.invgauss.fit(WindElsenborn)[1]
-scaleInvGaussElsenborn = stats.invgauss.fit(WindElsenborn)[2]
+shapeInvGaussElsenborn, locationInvGaussElsenborn, scaleInvGaussElsenborn = stats.invgauss.fit(electroCapa)
 
-x = np.linspace (0, 150, 150)
+x = np.linspace (0, etendue, etendue) #creat an array with of percentil
 
-gammaPdf = stats.gamma.pdf(x, shapeGammaElsenborn, scale=scaleGammaElsenborn)
-invGaussPdf = stats.invgauss.pdf(x, shapeInvGaussElsenborn, scale=scaleInvGaussElsenborn)
+gammaPdf = stats.gamma.pdf(x, shapeGammaElsenborn, loc=locationGammaElsenborn, scale=scaleGammaElsenborn) #store all the value of the gamma pdf for each percitil
+invGaussPdf = stats.invgauss.pdf(x, shapeInvGaussElsenborn, loc=locationInvGaussElsenborn, scale=scaleInvGaussElsenborn) #store all the value of the inverte gauss pdf for each percentil
+
+#creat an array that has the amount of value for each value of MW rounded
+def makeArray(etendue):
+    a = np.empty(etendue)
+    for i in range(etendue-1):
+        a[round(electroCapa[i])] += 1
+
+    return a
+
+totalErrorGamma = 0
+totalErrorInvGauss = 0
+#compute de error by taking the difference between the curve and the real value (rounded)
+for i in range(etendue):
+    totalErrorGamma += (gammaPdf[i] - makeArray(etendue)[i])**2
+    totalErrorInvGauss += (invGaussPdf[i] - makeArray(etendue)[i])**2
+
+print("total error gamma: ", totalErrorGamma)
+print("total error inverte Gauss: ",totalErrorInvGauss)
+
 
 plt.plot(x, gammaPdf)
 plt.plot(x, invGaussPdf)
