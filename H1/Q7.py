068f73f9c51a58a25411e9662002164dbaac3632
Theo aka tetonMAx <theodore.cousin@student.uclouvain.be>
2022-10-23 15:09:39 +0200

    writing q7 in the notebook

--- H1/Q7.py
+++ H1/Q7.py
@@ -1,5 +1,4 @@
 from math import *
-from turtle import st
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,57 +8,39 @@ import random as rn
 from Q4 import WindBeauvechain
 from Q4 import WindElsenborn
 
-#only keep 500 wind speeds
-winds_b = WindBeauvechain[0:500]
-winds_e = WindElsenborn[0:500]
+def Q7(array, place):
 
-#number of bootstrappings
-M = 1000
+    #only keep 500 wind speeds
+    winds = array[:500]
 
-#arrays that will contain the gamma parameters for each resample
-alphab =[]
-betab = []
+    #number of bootstrappings
+    M = 1000
 
-#take the wind speed sample, shuffle it and then estimate the parameters with an MLE
-for m in np.arange(0,M):
-    datm = rn.choices(population=winds_b,k=len(winds_b))
-    params = sc.gamma.fit(data=datm,scale=1)
-    alphab.append(params[0])
-    betab.append(params[2])
+    #arrays that will contain the gamma parameters for each resample
+    alpha = []
+    beta = []
+
+    #take the wind speed sample, shuffle it and then estimate the parameters with an MLE
+    for m in np.arange(0,M):
+        datm = rn.choices(population=winds,k=len(winds))
+        params = sc.gamma.fit(data=datm,scale=1)
+        alpha.append(params[0])
+        beta.append(params[2])
 
 
-alphab = np.sort(alphab)
-betab = np.sort(betab)
+    alpha = np.sort(alpha)
+    beta = np.sort(beta)
 
-#taking the lower and upper bounds of the 5% confidence interval
-alphab_begin = alphab[25]
-alphab_end = alphab[974]
+    #taking the lower and upper bounds of the 5% confidence interval
+    alpha_begin = alpha[25]
+    alpha_end = alpha[974]
 
-betab_begin = betab[25]
-betab_end = betab[974]
+    beta_begin = beta[25]
+    beta_end = beta[974]
 
-print("Beauvechain :")
-print("Aplha 5% confidence interval: [",alphab_begin,":",alphab_end,"]")
-print("Beta 5% confidence interval: [",betab_begin,":",betab_end,"] \n")
+    print(f"{place} :")
+    print("Aplha 5% confidence interval: [",alpha_begin,":",alpha_end,"]")
+    print("Beta 5% confidence interval: [",beta_begin,":",beta_end,"] \n")
 
-#do the same thing all over again for Elsenborn
-alphae =[]
-betae = []
-for m in np.arange(0,M):
-    datm = rn.choices(population=winds_e,k=len(winds_e))
-    params = sc.gamma.fit(data=datm,scale=1)
-    alphae.append(params[0])
-    betae.append(params[2])
-
-alphae = np.sort(alphae)
-betae = np.sort(betae)
-
-alphae_begin = alphae[25]
-alphae_end = alphae[974]
-
-betae_begin = betae[25]
-betae_end = betae[974]
-
-print("Elsenborn :")
-print("Aplha 5% confidence interval: [",alphae_begin,":",alphae_end,"]")
-print("Beta 5% confidence interval: [",betae_begin,":",betae_end,"] \n")
+Q7(WindBeauvechain, "Beauvechain")
+Q7(WindElsenborn, "Elsenborn")
