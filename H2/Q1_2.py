769cd592fc01342841b8ae4fa9c1c43b711cc1f3
Theo aka tetonMAx <theodore.cousin@student.uclouvain.be>
2022-11-09 12:24:23 +0100

    Q1 feat dieg

---
+++ H2/Q1_2.py
@@ -0,0 +1,15 @@
+from imports import *
+
+df = pd.read_csv("/home/jabier/Desktop/Stats/Hackatons/Hackathons/H2/Data_energy_load.csv", sep=";")
+
+df["Date"] = pd.to_datetime(df["Date"])
+
+#C'est juste les loads en fonction du temps
+LOAD_DATA = np.array(df["Load"])
+
+plt.plot([i for i in range(len(df["Date"]))], LOAD_DATA)
+plt.xlabel("Time")
+plt.ylabel("Consumption")
+plt.show()
+
+print(df)
