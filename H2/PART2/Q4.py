4b352f3994a7bfb48cf6361501ce14394fee2b88
thomasrixen <thomas.rixen@student.uclouvain.be>
2022-11-12 21:49:04 +0100

    correction date

--- H2/PART2/Q4.py
+++ H2/PART2/Q4.py
@@ -3,7 +3,7 @@ from Q1 import df2
 
 #dates
 time1 = pd.Timestamp('20150102')
-time2 = pd.Timestamp('20180112')
+time2 = pd.Timestamp('20181201')
 time3 = pd.Timestamp('20181202')
 time4 = pd.Timestamp('20181231')
 
@@ -11,8 +11,8 @@ time4 = pd.Timestamp('20181231')
 training_set = df2.loc[df2["Date"] <= time2]
 test = df2.loc[df2["Date"] >= time3].loc[df2["Date"] <= time4]
 
-#print(training_set)
-for i in df2["Date"]: print(i)
+print(training_set)
+#for i in df2["Date"]: print(i)
 
-#print(test)
+print(test)
 
